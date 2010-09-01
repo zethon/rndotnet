6e07ed7a4493d0a7f1372165340144dc7ef51a9f
aclaure@gmail.com <aclaure@gmail.com@dbd61385-8a2f-4f18-ba9e-8a4471b0b91e>
2010-09-01 14:56:46 +0000

    added license info, logo information, and dynamically name the executable

--- Program.cs
+++ Program.cs
@@ -1,8 +1,26 @@
-﻿using System;
+﻿/*
+ * Copyright 2010 Adalid Claure
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License"); 
+ * you may not use this file except in compliance with the License. 
+ * You may obtain a copy of the License at 
+ * 
+ *      http://www.apache.org/licenses/LICENSE-2.0 
+ *      
+ * Unless required by applicable law or agreed to in writing, software 
+ * distributed under the License is distributed on an "AS IS" BASIS, 
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. 
+ * See the License for the specific language governing permissions and 
+ * limitations under the License. 
+*/
+
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.IO;
+using System.Reflection;
+using System.Diagnostics;
 
 namespace RubiksNotation
 {
@@ -10,6 +28,14 @@ namespace RubiksNotation
     {
         static void Main(string[] args)
         {
+            Assembly asm = Assembly.GetExecutingAssembly();
+            string strTitle = ((AssemblyTitleAttribute)asm.GetCustomAttributes(typeof(AssemblyTitleAttribute), false)[0]).Title;
+            string strCopyright = ((AssemblyCopyrightAttribute)asm.GetCustomAttributes(typeof(AssemblyCopyrightAttribute), false)[0]).Copyright;
+            FileVersionInfo verInfo = FileVersionInfo.GetVersionInfo("rcnc.exe");
+            
+            Console.WriteLine("{0} version {1}",strTitle,verInfo.FileVersion);
+            Console.WriteLine("{0}. All rights reserved.",strCopyright); 
+
             if (args.Length != 1)
             {
                 Console.WriteLine("Usage: rcnc progran.rcn");
@@ -25,7 +51,7 @@ namespace RubiksNotation
                 }
 
                 Parser p = new Parser(scanner.Tokens);
-                CodeGen gen = new CodeGen(p.Result, @"test.exe");
+                CodeGen gen = new CodeGen(p.Result, Path.GetFileNameWithoutExtension(args[0]) + ".exe");
             }
             catch (Exception ex)
             {
